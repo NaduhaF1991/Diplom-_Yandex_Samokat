04e8c2178c60be041c6671fa06c25b2a4d66a90f
Nadezhda <naduha1991@yandex.ru>
2023-10-23 22:01:44 +0300

    first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+#Филиппова Надежда 9-ая кагорта дипломный проект Инженер по тестированию плюс
+URL_SERVER = 'https://a73fc649-8e2d-4e0a-bc76-eb03fdd60d88.serverhub.praktikum-services.ru'
+CREATE_ORDERS = '/api/v1/orders'
+GET_ORDERS = '/v1/orders/track'
